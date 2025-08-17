50f4333e46d4380844e01df7a8401bb4d39324ad
msnthr0pe <pavlid66@gmail.com>
2025-08-17 18:57:22 +0300

    Project fix transfer

---
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import android.content.res.Resources
+import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val itemClickListener: View.OnClickListener = object : View.OnClickListener{override fun onClick(v: View?)
+        {
+            startActivity(Intent(this@MainActivity, SearchActivity::class.java))
+        }
+        }
+
+        val searchBtn = findViewById<Button>(R.id.search_btn).setOnClickListener(itemClickListener)
+        val mediaBtn = findViewById<Button>(R.id.media_btn).setOnClickListener {
+            startActivity(Intent(this, MediaActivity::class.java))
+        }
+        val settingsBtn = findViewById<Button>(R.id.settings_btn).setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+}
