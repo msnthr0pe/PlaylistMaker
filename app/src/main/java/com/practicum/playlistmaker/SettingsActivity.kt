0b21f334aef6b048af89b3bc256cbda9940a3427
msnthr0pe <pavlid66@gmail.com>
2025-08-18 22:48:56 +0300

    Buggy theme change

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,9 +2,12 @@ package com.practicum.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.util.Log
 import android.widget.ImageView
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,18 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         val settingsBtn = findViewById<ImageView>(R.id.back_settings_btn)
+
+        val darkThemeSwitch = findViewById<SwitchCompat>(R.id.dark_theme_switch)
+
+
+        darkThemeSwitch.setOnCheckedChangeListener { _, isNightTheme ->
+            Log.d("themeInfo", "$isNightTheme")
+            if (isNightTheme) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
         settingsBtn.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
