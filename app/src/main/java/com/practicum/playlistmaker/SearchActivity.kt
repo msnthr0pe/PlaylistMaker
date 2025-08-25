b37a49ce579fbcfbdcd2efeadf44abe6079c5be7
msnthr0pe <pavlid66@gmail.com>
2025-08-25 18:19:43 +0300

    Text watcher

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,6 +1,12 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +16,46 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchField = findViewById<EditText>(R.id.search_et)
+        val clearBtn = findViewById<ImageView>(R.id.clear_text_btn)
+
+        clearBtn.setOnClickListener {
+            searchField.setText("")
+        }
+
+        clearBtn.visibility = View.GONE
+
+        findViewById<ImageView>(R.id.back_search_btn).setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        val textWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int,
+            ) {}
+
+            override fun onTextChanged(
+                p0: CharSequence?,
+                p1: Int,
+                p2: Int,
+                p3: Int,
+            ) {
+                if (searchField.text.isNotEmpty()) {
+                    clearBtn.visibility = View.VISIBLE
+                } else {
+                    clearBtn.visibility = View.GONE
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+
+        }
+
+        searchField.addTextChangedListener(textWatcher)
     }
 }
