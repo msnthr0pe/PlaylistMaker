1e42d330a1498305fd1a8d26a9f3a7c692548d17
msnthr0pe <pavlid66@gmail.com>
2025-09-26 13:45:59 +0300

    Updated glide dependencies versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
 }
 
 dependencies {
-    implementation ("com.github.bumptech.glide:glide:4.14.2")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.14.2")
+    implementation (libs.glide)
+    annotationProcessor (libs.compiler)
 
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
