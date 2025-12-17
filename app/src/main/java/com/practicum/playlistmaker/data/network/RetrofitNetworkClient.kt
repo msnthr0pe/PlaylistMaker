3ac57bf31487b4fa115679b0e6afb06a5da31b1c
i.pavlovskiy <i.pavlovskiy@vkteam.ru>
2025-12-17 22:47:19 +0300

    Fixed crash caused by absence internet

--- app/src/main/java/com/practicum/playlistmaker/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/network/RetrofitNetworkClient.kt
@@ -5,6 +5,7 @@ import com.practicum.playlistmaker.data.dto.Response
 import com.practicum.playlistmaker.data.dto.TrackSearchRequest
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.Exception
 
 object RetrofitNetworkClient : NetworkClient {
     private const val BASE_URL = "https://itunes.apple.com/"
@@ -18,11 +19,13 @@ object RetrofitNetworkClient : NetworkClient {
 
     override fun doRequest(dto: Any): Response {
         if (dto is TrackSearchRequest) {
-            val resp = searchMusicApi.search(text = dto.text).execute()
-
-            val body = resp.body() ?: Response()
-
-            return body.apply { resultCode = resp.code() }
+            try {
+                val resp = searchMusicApi.search(text = dto.text).execute()
+                val body = resp.body() ?: Response()
+                return body.apply { resultCode = resp.code() }
+            } catch (_: Exception){
+                return Response()
+            }
         } else {
             return Response().apply { resultCode = 400 }
         }
