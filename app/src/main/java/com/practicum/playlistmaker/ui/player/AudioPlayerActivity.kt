1697fd612e27b53255fdde22a595b5bc4fb0e910
i.pavlovskiy <i.pavlovskiy@vkteam.ru>
2026-01-10 23:51:32 +0300

    Safe point before PlayerViewModel integration

--- app/src/main/java/com/practicum/playlistmaker/ui/player/AudioPlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/ui/player/AudioPlayerActivity.kt
@@ -9,17 +9,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.practicum.playlistmaker.PlaylistUtil
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.ActivityAudioPlayerBinding
 import com.practicum.playlistmaker.domain.models.Track
 import com.practicum.playlistmaker.ui.player.viewmodel.PlayerHelper
+import com.practicum.playlistmaker.ui.player.viewmodel.PlayerViewModel
 
 class AudioPlayerActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAudioPlayerBinding
+    private lateinit var viewModel: PlayerViewModel
     private lateinit var playButton: ImageButton
     private lateinit var currentTrackTime: TextView
+    private lateinit var track: Track
     private var mainThreadHandler: Handler? = null
     private var playerHelper = PlayerHelper()
 
@@ -35,12 +39,18 @@ class AudioPlayerActivity : AppCompatActivity() {
         }
         mainThreadHandler = Handler(Looper.getMainLooper())
         setData()
+        //setViewModelObservers()
+        //preparePlayer()
         setOnClickListeners()
 
     }
 
     private fun setData() {
-        val track = intent.getSerializableExtra("Track") as Track
+        track = intent.getSerializableExtra("Track") as Track
+
+        val factory = PlayerViewModel.getFactory(track.previewUrl)
+        viewModel = ViewModelProvider(this, factory)[PlayerViewModel::class.java]
+
         val pic = PlaylistUtil.getHigherResolutionPic(track.artworkUrl100)
         val imageView = binding.playerSongImage
         PlaylistUtil.loadPicInto(this, pic, imageView)
@@ -66,6 +76,30 @@ class AudioPlayerActivity : AppCompatActivity() {
             {
                 setPlayButtonImage()
             })
+    }
+
+    private fun setViewModelObservers() {
+        with (viewModel) {
+            observePlayerState().observe(this@AudioPlayerActivity) {
+                when (it) {
+                    PlayerViewModel.PlayerState.DEFAULT -> Unit
+                    PlayerViewModel.PlayerState.PREPARED -> playButton.isEnabled = true
+                    PlayerViewModel.PlayerState.COMPLETED -> setPlayButtonImage()
+                }
+            }
+
+            observePlaying().observe(this@AudioPlayerActivity) {
+                setPlayButtonImage()
+            }
+
+            observeProgressTime().observe(this@AudioPlayerActivity) {
+                currentTrackTime.text = it
+            }
+        }
+    }
+
+    private fun preparePlayer() {
+        viewModel.preparePlayer(mainThreadHandler)
     }
 
     private fun setOnClickListeners() {
